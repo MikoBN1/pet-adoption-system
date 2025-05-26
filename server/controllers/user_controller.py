7e728dc1eb78538079ccffd9c2a8fe3f218e2baa
miko <baidildinmiras608@gmail.com>
2025-05-26 19:22:10 +0500

    feat: get all users

--- server/controllers/user_controller.py
+++ server/controllers/user_controller.py
@@ -1,15 +1,23 @@
+from typing import List
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
 from models.user_model import User
-from schemas.user_schema import User as UserSchema
+from schemas.user_schema import UserOut
+
 router = APIRouter(prefix="/user", tags=["user"])
+
+@router.get("/all", response_model=List[UserOut])
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+    return users
 
 @router.get("/{user_id}")
 def get_user(user_id: int, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.id == user_id).first()
-    print(user.name)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
+
 
