e4634616ffd6fb57384bf104052976caf77589cb
miko <baidildinmiras608@gmail.com>
2025-06-08 14:16:08 +0500

    feat: update and delete for pet

--- server/controllers/pet_controller.py
+++ server/controllers/pet_controller.py
@@ -3,11 +3,12 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
 from models.pet_model import Pet
+from schemas.pet_schema import Pet as PetResponse
 from schemas.pet_schema import PetBase, PetCreate
 
 router = APIRouter(prefix="/pets", tags=["pets"])
 
-@router.get("/all", response_model=List[PetBase])
+@router.get("/all", response_model=List[PetResponse])
 def get_pets(db: Session = Depends(get_db)):
     pets = db.query(Pet).all()
     return pets
@@ -16,7 +17,7 @@ def get_pets(db: Session = Depends(get_db)):
 def get_user(pet_id: int, db: Session = Depends(get_db)):
     pet = db.query(Pet).filter(Pet.id == pet_id).first()
     if not pet:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Pet not found")
     return pet
 
 @router.post("/new", response_model=PetBase)
@@ -26,6 +27,28 @@ def create_pet(pet_data: PetCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_pet)
     return new_pet
+
+@router.patch("/{pet_id}")
+def update_pet(pet_id: int, pet_data: PetBase, db: Session = Depends(get_db)):
+    update_data = pet_data.dict(exclude_unset=True)
+
+    result = db.query(Pet).filter(Pet.id == pet_id).update(update_data)
+    if result == 0:
+        raise HTTPException(status_code=404, detail="Pet not found")
+
+    db.commit()
+    updated_pet = db.query(Pet).filter(Pet.id == pet_id).first()
+    return updated_pet
+
+@router.delete("/{pet_id}")
+def delete_pet(pet_id: int, db: Session = Depends(get_db)):
+    pet = db.query(Pet).filter(Pet.id == pet_id).first()
+    if not pet:
+        raise HTTPException(status_code=404, detail="Pet not found")
+
+    db.delete(pet)
+    db.commit()
+    return {"message": "Pet deleted"}
 
 
 
